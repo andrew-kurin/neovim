a1d3269c311409f6108837b5d7b36f1da03eca44
Andrew Kurin <prigun@gmail.com>
2024-02-20 21:27:15 -0800

    Use gpt-4-turbo-preview in ChatGPT

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -12,6 +12,23 @@ return {
     local chatgpt = require("chatgpt")
     chatgpt.setup({
       api_key_cmd = "op item get OPENAI_MBAIR --account my.1password.com --fields label=password",
+      openai_params = {
+        model = "gpt-4-turbo-preview",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 300,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+      openai_edit_params = {
+        model = "gpt-4-turbo-preview",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
     })
     vim.keymap.set(
       "n",
